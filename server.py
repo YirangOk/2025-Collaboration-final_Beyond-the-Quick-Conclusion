e2684dd01c7d45112684763398755531609bec59
YirangOk <paperwithok@gmail.com>
2025-05-19 15:05:38 +0200

    ex

--- server.py
+++ server.py
@@ -122,4 +122,4 @@ def resources():
     return jsonify({"resources":items})
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
